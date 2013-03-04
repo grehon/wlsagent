9635425d7f28637f891990a252d6c3586cdf52b6
yann.lambret@gmail.com <yann.lambret@gmail.com@e99450f6-1368-aaa9-bd5f-839484830218>
2013-03-04 20:41:51 +0000

    Moving files to the new 'core' package.

---
+++ trunk/src/net/wait4it/nagios/wlsagent/core/WlsAgent.java
@@ -0,0 +1,60 @@
+/**
+ * This file is part of Wlsagent.
+ *
+ * Wlsagent is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wlsagent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wlsagent. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package net.wait4it.nagios.wlsagent.core;
+
+
+import java.net.InetSocketAddress;
+
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * @author Yann Lambret
+ */
+public class WlsAgent {
+
+    private WlsAgent() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        InetSocketAddress address = new InetSocketAddress(host, port);
+        Server server = new Server(address);
+
+        // Timout for incoming HTTP requests
+        for (Connector connector : server.getConnectors()) {
+            connector.setMaxIdleTime(55000);
+        }
+
+        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        handler.setContextPath("/wlsagent");
+        server.setHandler(handler);
+
+        handler.addServlet(new ServletHolder(new WlsServlet()), "/*");
+
+        server.start();
+        server.join();
+    }
+
+}
